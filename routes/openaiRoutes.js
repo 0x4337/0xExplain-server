16f3909b0ea944485c6d02819da336c969920c72
Vantria <s1spam118@gmail.com>
2023-03-29 17:08:08 +0100

    fix various bugs

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,7 +1,7 @@
-const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
 const { Configuration, OpenAIApi } = require("openai");
-
-const router = Router();
+require("dotenv").config();
 
 // sets the api key for the openai api to the value of the environment variable
 const config = new Configuration({
@@ -11,23 +11,38 @@ const config = new Configuration({
 // creates an instance of the openai api with the config
 const openai = new OpenAIApi(config);
 
-router.post("/explain", async (req, res) => {
+router.post("/contract", async (req, res) => {
   // tries to make an API call to OpenAI and catch any errors
+
+  console.log(req.body);
+
   try {
     // get the sourceCode from the request body
     const sourceCode = req.body.sourceCode;
+    // const prompt = `Your job is to provide a detailed analysis of the following ethereum smart contract, explain what each function does and point out any dangerous features for users interacting with it, if any: ${sourceCode}`;
+    const prompt = `Say: Hello, im GPT-4 and im ready to role!!!!!!!`;
+    // const testPrompt = "say hello world";
+
+    console.log("before calling OpenAI API"); // Add this line
 
     // creates a completion object with the sourceCode as the prompt
     const { data } = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      message: [{ role: "user", content: sourceCode }],
+      //   model: "gpt-3.5-turbo",
+      model: "gpt-4",
+      messages: [{ role: "user", content: prompt }],
     });
 
+    console.log(data);
+    console.log("After calling OpenAI API");
+
     // respond with the data from the OpenAI API call
+    res.json({ explanation: data.choices[0].message.content });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "An error occurred", details: error.message });
+    console.log(error);
+    res.status(500).json({
+      message: "Error whilst trying to get explination",
+      details: error.message,
+    });
   }
 });
 
--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", openaiRoutes);
+app.use("/api/openai", openaiRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
